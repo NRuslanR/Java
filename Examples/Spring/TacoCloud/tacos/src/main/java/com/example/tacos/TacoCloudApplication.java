898469f920d1bb90ade6820903aa9ea8cca8bf72
Ruslan <brainsort@yandex.ru>
2022-01-01 10:58:43 +0400

    Spring Boot Taco Cloud

--- Examples/Spring/TacoCloud/tacos/src/main/java/com/example/tacos/TacoCloudApplication.java
+++ Examples/Spring/TacoCloud/tacos/src/main/java/com/example/tacos/TacoCloudApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 public class TacoCloudApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(TacoCloudApplication.class, args);
 
 	}
